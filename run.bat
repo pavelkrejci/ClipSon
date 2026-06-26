69bacc4442eb9481e902da49605ce2e6023acae3
A809826 <pavel.krejci@atos.net>
2026-06-26 17:37:38 +0200

    update

--- run.bat
+++ run.bat
@@ -6,4 +6,3 @@ echo Current directory: %CD%
 powershell.exe -NoProfile -Command "Get-ChildItem -Path . -Filter '*.ps1' -File -Recurse | Unblock-File"
 powershell.exe -NoProfile -File ".\\clipson.ps1"
 
-pause
